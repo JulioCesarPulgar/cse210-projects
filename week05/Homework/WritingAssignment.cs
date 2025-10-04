5825a58da3eb3cf30cef3cebf4a58e1f82458284
JulioCesarPulgar <pulgarjuliocesar@gmail.com>
2025-10-03 23:42:01 -0400

    update

---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        // Use the public getter from base class to access student name
+        return $"{_title} by {GetStudentName()}";
+    }
+}
